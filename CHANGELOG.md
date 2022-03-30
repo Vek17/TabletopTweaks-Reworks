2f023a338309e12d8bb9aa7a46398dc8592676c4
Sean Petrie <sean.petrie17@gmail.com>
2022-03-29 20:21:00 -0500

    Updated knowledge arcana

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## Version 1.1.0
+Trickster Rework
+
 ## Version 1.0.0
 This has been split and now depends on [TabletopTweaks-Core](https://github.com/Vek17/TabletopTweaks-Core/releases).
 
--- TabletopTweaks-Reworks/Patches/Trickster.cs
+++ TabletopTweaks-Reworks/Patches/Trickster.cs
@@ -1,17 +1,12 @@
 ﻿using HarmonyLib;
 using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.Items.Ecnchantments;
 using Kingmaker.Blueprints.JsonSystem;
-using Kingmaker.Enums;
+using Kingmaker.Designers.Mechanics.Facts;
 using Kingmaker.UnitLogic.Abilities.Blueprints;
-using Kingmaker.UnitLogic.ActivatableAbilities;
 using Kingmaker.UnitLogic.Buffs.Blueprints;
 using Kingmaker.UnitLogic.FactLogic;
-using Kingmaker.UnitLogic.Mechanics.Actions;
-using Kingmaker.Utility;
-using System.Linq;
-using TabletopTweaks.Core.NewComponents;
-using TabletopTweaks.Core.NewComponents.OwlcatReplacements;
 using TabletopTweaks.Core.Utilities;
 using static TabletopTweaks.Reworks.Main;
 
@@ -24,11 +19,93 @@ namespace TabletopTweaks.Reworks.Patches {
             static void Postfix() {
                 if (Initialized) return;
                 Initialized = true;
-                TTTContext.Logger.LogHeader("Azata Rework");
+                TTTContext.Logger.LogHeader("Trickster Rework");
 
+                PatchTricksterKnowledgeArcana2();
+                PatchTricksterKnowledgeArcana3();
                 PatchTricksterStealthAbilityName();
                 PatchTricksterStealth1();
                 PatchTricksterStealth2();
+            }
+            static void PatchTricksterKnowledgeArcana2() {
+                if (TTTContext.Homebrew.MythicReworks.Trickster.IsDisabled("TricksterKnowledgeArcana2")) { return; }
+                var TricksterKnowledgeArcanaTier2Feature = BlueprintTools.GetBlueprint<BlueprintFeature>("7bbd9f681440a294382b527a554e419d");
+
+                var Agile = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("a36ad92c51789b44fa8a1c5c116a1328");
+                var Bleed = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("ac0108944bfaa7e48aa74f407e3944e3");
+                var Corrosive = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("633b38ff1d11de64a91d490c683ab1c8");
+                var CruelEnchantment = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("629c383ffb407224398bb71d1bd95d14");
+                var Flaming = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("30f90becaaac51f41bf56641966c4121");
+                var Frost = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("421e54078b7719d40915ce0672511d0b");
+                var Furious = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("b606a3f5daa76cc40add055613970d2a");
+                var Furyborn = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("091e2f6b2fad84a45ae76b8aac3c55c3");
+                var GhostTouch = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("47857e1a5a3ec1a46adf6491b1423b4f");
+                var Heartseeker = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("e252b26686ab66241afdf33f2adaead6");
+                var Keen = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("102a9c8c9b7a75e4fb5844e79deaf4c0");
+                var Necrotic = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("bad4134798e182c4487819dce9b43003");
+                var Radiant = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("5ac5c88157f7dde48a2a5b24caf40131");
+                var Shock = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("7bda5277d36ad114f9f9fd21d0dab658");
+                var Thundering = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("690e762f7704e1f4aa1ac69ef0ce6a96");
+                var ViciousEnchantment = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("a1455a289da208144981e4b1ef92cc56");
+
+                TricksterKnowledgeArcanaTier2Feature
+                    .GetComponent<TricksterArcanaAdditionalEnchantments>()
+                    .WeaponEnchantments = new BlueprintWeaponEnchantmentReference[] {
+                        Keen,
+                        Agile,
+                        Frost,
+                        Flaming,
+                        Corrosive,
+                        Shock,
+                        GhostTouch,
+                        Heartseeker,
+                        CruelEnchantment,
+                        Bleed,
+                        Furious,
+                        Thundering,
+                        Necrotic,
+                        Radiant,
+                        Furyborn,
+                        ViciousEnchantment
+                    };
+
+                TTTContext.Logger.LogPatch("Patched", TricksterKnowledgeArcanaTier2Feature);
+            }
+            static void PatchTricksterKnowledgeArcana3() {
+                if (TTTContext.Homebrew.MythicReworks.Trickster.IsDisabled("TricksterKnowledgeArcana3")) { return; }
+                var TricksterKnowledgeArcanaTier3Feature = BlueprintTools.GetBlueprint<BlueprintFeature>("5e26c673173e423881e318d2f0ae84f0");
+
+                var Axiomatic = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("0ca43051edefcad4b9b2240aa36dc8d4");
+                var BaneLiving = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("e1d6f5e3cd3855b43a0cb42f6c747e1c");
+                var BrilliantEnergy = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("66e9e299c9002ea4bb65b6f300e43770");
+                var Deteriorative = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("bbe55d6e76b973d41bf5abeed643861d");
+                var ElderBrass = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("0a8f3559cfcc4d38961bd9658d026cc8");
+                var ElderCorrosive2d6 = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("55eece8fead0448aac01c44f37ea065a");
+                var ElderIce2d6 = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("c4c701aee76742188477a6f24505c222");
+                var ElderShock2d6 = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("5573c979b9dc403684166fe6e1c31c15");
+                var Holy = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("28a9964d81fedae44bae3ca45710c140");
+                var Speed = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("f1c0c50108025d546b2554674ea1c006");
+                var Ultrasound = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("582849db96824254ebcc68f0b7484e51");
+                var Vorpal = BlueprintTools.GetBlueprintReference<BlueprintWeaponEnchantmentReference>("2f60bfcba52e48a479e4a69868e24ebc");
+
+                TricksterKnowledgeArcanaTier3Feature
+                    .GetComponent<TricksterArcanaAdditionalEnchantments>()
+                    .WeaponEnchantments = new BlueprintWeaponEnchantmentReference[] {
+                        ElderCorrosive2d6,
+                        ElderBrass,
+                        ElderIce2d6,
+                        ElderShock2d6,
+                        Axiomatic,
+                        Holy,
+                        Speed,
+                        Ultrasound,
+                        BrilliantEnergy,
+                        Deteriorative,
+                        BaneLiving,
+                        Vorpal
+                    };
+
+                TTTContext.Logger.LogPatch("Patched", TricksterKnowledgeArcanaTier3Feature);
             }
             static void PatchTricksterStealthAbilityName() {
                 if (TTTContext.Homebrew.MythicReworks.Trickster.IsDisabled("TricksterStealthAbilityName")) { return; }
@@ -53,7 +130,7 @@ namespace TabletopTweaks.Reworks.Patches {
                     "You can enter stealth during combat as a move action. This stealth is not broken by a single enemy " +
                     "detecting you — instead it acts similar to the invisibility spell, giving you total concealment against " +
                     "all creatures that did not succeed on a Perception check against you.\n" +
-                    "This \"invisibility\" cannot be seen through by divination magic such as True Seeing, See Invisability, or Thoughtsense.");
+                    "This \"invisibility\" cannot be seen through by divination magic such as true seeing, see invisability, or thoughtsense.");
                 TricksterStealthTier1Buff.AddComponent<AddFacts>(c => {
                     c.m_Facts = new BlueprintUnitFactReference[] { DivinationImmunityFeature };
                 });
